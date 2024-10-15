11d86ab07b7c992fef5877fa430ff1a055e2a717
Sỳ Phát Thành <phatthanh@MacBook-Pro-cua-Sy.local>
2024-10-15 16:12:57 +0700

    Error Handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const doctorRouter = require('./routes/doctorRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -19,5 +21,11 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/doctors', doctorRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,135 +1,104 @@
 const Doctor = require('../models/doctorModel');
-const APIFeatures = require('../utils/APIFeatures');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
-exports.getAllDoctors = async (req, res) => {
-    try {
-        const features = new APIFeatures(
-            Doctor.find().populate('owner'),
-            req.query
-        )
-            .filter()
-            .sort()
-            .limitFields()
-            .paginate();
+exports.getAllDoctors = catchAsync(async (req, res, next) => {
+    const features = new APIFeatures(Doctor.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
 
-        const doctors = await features.query;
+    const doctors = await features.query;
 
-        res.status(200).json({
-            status: 'success',
-            requestedAt: req.requestTime,
-            total: doctors.length,
-            data: {
-                doctors
-            }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
+    res.status(200).json({
+        status: 'success',
+        requestedAt: req.requestTime,
+        total: doctors.length,
+        data: {
+            doctors
+        }
+    });
+});
+
+exports.getDoctor = catchAsync(async (req, res, next) => {
+    const doctor = await Doctor.findById(req.params.id).populate('owner');
+
+    if (!doctor) {
+        return next(new AppError('Can not found doctor with that id', 404));
     }
-};
 
-exports.getDoctor = async (req, res) => {
-    try {
-        const doctor = await Doctor.findById(req.params.id).populate('owner');
+    res.status(200).json({
+        status: 'success',
+        data: {
+            doctor
+        }
+    });
+});
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                doctor
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        });
+exports.createDoctor = catchAsync(async (req, res, next) => {
+    const newDoctor = await Doctor.create(req.body);
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            doctor: newDoctor
+        }
+    });
+});
+
+exports.updateDoctor = catchAsync(async (req, res, next) => {
+    const doctor = await Doctor.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if (!doctor) {
+        return next(new AppError('Can not found doctor with that id', 404));
     }
-};
 
-exports.createDoctor = async (req, res) => {
-    try {
-        const newDoctor = await Doctor.create(req.body);
+    res.status(200).json({
+        status: 'success',
+        data: {
+            doctor
+        }
+    });
+});
 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                doctor: newDoctor
-            }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
+exports.deleteDoctor = catchAsync(async (req, res, next) => {
+    const doctor = await Doctor.findByIdAndDelete(req.params.id);
+
+    if (!doctor) {
+        return next(new AppError('Can not found doctor with that id', 404));
     }
-};
 
-exports.updateDoctor = async (req, res) => {
-    try {
-        const doctor = await Doctor.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
 
-        res.status(200).json({
-            status: 'success',
-            data: {
-                doctor
+exports.getDoctorStats = catchAsync(async (req, res, next) => {
+    const stats = await Doctor.aggregate([
+        {
+            $match: { experience: { $gte: 2 } }
+        },
+        {
+            $group: {
+                // _id: '$specialty',
+                _id: null,
+                totalDoctors: { $sum: 1 },
+                avgExperience: { $avg: '$experience' },
+                avgFee: { $avg: '$fee' }
             }
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
-};
+        }
+    ]);
 
-exports.deleteDoctor = async (req, res) => {
-    try {
-        await Doctor.findByIdAndDelete(req.params.id);
-
-        res.status(204).json({
-            status: 'success',
-            data: null
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        });
-    }
-};
-
-exports.getDoctorStats = async (req, res) => {
-    try {
-        const stats = await Doctor.aggregate([
-            {
-                $match: { experience: { $gte: 2 } }
-            },
-            {
-                $group: {
-                    // _id: '$specialty',
-                    _id: null,
-                    totalDoctors: { $sum: 1 },
-                    avgExperience: { $avg: '$experience' },
-                    avgFee: { $avg: '$fee' }
-                }
-            }
-        ]);
-
-        res.status(200).json({
-            status: 'success',
-            data: {
-                stats
-            }
-        });
-    } catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err
-        });
-    }
-};
+    res.status(200).json({
+        status: 'success',
+        data: {
+            stats
+        }
+    });
+});
